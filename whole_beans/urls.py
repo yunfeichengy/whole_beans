d2de81b273d757c3558ecb8bb2bb45978e33fc6a
phil <cheng.yunfei16@gmail.com>
2020-04-23 02:52:28 -0700

    enable chat

--- whole_beans/urls.py
+++ whole_beans/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', include('post.urls')),
+    path('chat/', include('chat.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
